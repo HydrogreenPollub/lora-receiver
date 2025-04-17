11c02c2da49ba9157ebccebf27cac38609baba16
lizard123137 <lizard123137@gmail.com>
2025-04-17 23:51:43 +0200

    added flatbuffers as a module

--- desktop/gui.py
+++ desktop/gui.py
@@ -1,7 +1,16 @@
+import serial
 import tkinter as tk
+
+from flatbuffers import TSData
 
 def read_lora():
     print("Reading from LORA")
+
+    # TODO find right device
+    with serial.Serial("/dev/ttyS0", 125200, timeout=1) as s:
+        if s.in_waiting > 0:
+            raw_data = s.read(s.in_waiting)
+            data = TSData.TSData.GetRootAs(raw_data, 0)
 
 root = tk.Tk()
 root.title("LORA reader")
